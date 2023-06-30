fc864dcb896e00c2ab08a4ca3b36daf5e96ca63b
Tuvedunyel <mightygodx@gmail.com>
2023-06-30 13:50:50 +0200

    media player

--- src/app/[slug]/Client.tsx
+++ src/app/[slug]/Client.tsx
@@ -4,19 +4,48 @@ import Image from "next/image";
 import { PageType, TemoignagesType } from "./page";
 import { Dispatch, SetStateAction, useEffect, useState } from "react";
 
-const MediaPlayer = (
-  temoin: TemoignagesType,
-  showModal: boolean,
-  setShowModal: Dispatch<SetStateAction<boolean>>
-) => {
+const MediaPlayer = ({
+  temoin,
+  showModal,
+  setShowModal,
+}: {
+  temoin: TemoignagesType;
+  showModal: boolean;
+  setShowModal: Dispatch<SetStateAction<boolean>>;
+}) => {
   return (
     <>
       {showModal && (
         <section id="media">
           <div className="container">
             <button onClick={() => setShowModal(false)}>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1276.9 768">
+                <path
+                  id="hexa"
+                  d="M948.6 761.8L620.4 572.3v-379L948.6 3.7l328.3 189.5v379L948.6 761.8zM637.2 562.6l311.5 179.8 311.5-179.8V202.9L948.6 23.1 637.2 202.9v359.7z"
+                ></path>
+                <path
+                  id="arrow"
+                  d="M562.1 239l-88.3-88.3L242.9 380l231.7 234.8 88.3-88.4-86-89.1h328V322.7h-328l85.2-83.7z"
+                  fill="#e3775b"
+                ></path>
+              </svg>
               Retour aux références
             </button>
+            <iframe
+              width="560"
+              height="315"
+              src={`https://www.youtube-nocookie.com/embed/${temoin.id_video}`}
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowFullScreen
+            ></iframe>
+            <h3>
+              {temoin.nom}
+              <span>{temoin.job}</span>
+              {temoin.societe}
+            </h3>
           </div>
         </section>
       )}
@@ -27,21 +56,6 @@ const MediaPlayer = (
 export default function Client({ page }: { page: PageType }) {
   const [showModal, setShowModal] = useState<boolean>(false);
 
-  const MediaPlayer = (temoin: TemoignagesType) => {
-    return (
-      <>
-        {showModal && (
-          <section id="media">
-            <div className="container">
-              <button onClick={() => setShowModal(false)}>
-                Retour aux références
-              </button>
-            </div>
-          </section>
-        )}
-      </>
-    );
-  };
   return (
     <main id="client">
       <section
@@ -79,7 +93,10 @@ export default function Client({ page }: { page: PageType }) {
         <ul className="clients">
           {page.acf.temoignage.map((temoin, index) => (
             <li key={index}>
-              <div className={temoin.id_video ? "photo video" : "photo"}>
+              <div
+                className={temoin.id_video ? "photo video" : "photo"}
+                onClick={() => setShowModal(true)}
+              >
                 <Image
                   src={temoin.portrait.url}
                   alt={temoin.portrait.alt}
@@ -102,6 +119,13 @@ export default function Client({ page }: { page: PageType }) {
               <div className="desc">
                 <p>{temoin.description}</p>
               </div>
+              {temoin.id_video && (
+                <MediaPlayer
+                  temoin={temoin}
+                  showModal={showModal}
+                  setShowModal={setShowModal}
+                />
+              )}
             </li>
           ))}
         </ul>
